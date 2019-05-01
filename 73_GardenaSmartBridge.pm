fcac1b39de5f113ff58af7941fc03ac5733c4184
Marko Oldenburg <leongaultier@gmail.com>
2019-05-01 18:00:24 +0200

    add first code for pressure pump

--- 73_GardenaSmartBridge.pm
+++ 73_GardenaSmartBridge.pm
@@ -386,10 +386,10 @@ sub Write($@) {
         }
     );
 
-    Log3 $name, 4,
-"GardenaSmartBridge ($name) - Send with URL: $hash->{URL}$uri, HEADER: secret!, DATA: secret!, METHOD: $method";
-#     Log3 $name, 3,
-# "GardenaSmartBridge ($name) - Send with URL: $hash->{URL}$uri, HEADER: $header, DATA: $payload, METHOD: $method";
+#     Log3($name, 4,
+#         "GardenaSmartBridge ($name) - Send with URL: $hash->{URL}$uri, HEADER: secret!, DATA: secret!, METHOD: $method");
+    Log3($name, 3,
+        "GardenaSmartBridge ($name) - Send with URL: $hash->{URL}$uri, HEADER: $header, DATA: $payload, METHOD: $method");
 }
 
 sub ErrorHandling($$$) {
@@ -985,6 +985,21 @@ sub createHttpValueStrings($@) {
               . $abilities
               . '/properties/watering_timer_'
               . $valve_id;
+
+        }
+        elsif ( defined($abilities)
+            and defined($payload)
+            and $abilities eq 'manual_watering' )
+        {
+            my $valve_id;
+            $method = 'PUT';
+
+            $uri .=
+                '/devices/'
+              . $deviceId
+              . '/abilities/'
+              . $abilities
+              . '/properties/manual_watering_timer';
 
         }
         elsif ( defined($abilities)
