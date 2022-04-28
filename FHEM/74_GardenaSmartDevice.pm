78e9906e6b2f42694c0c4484a87032572320df1a
Sebastian <em@ail.tld>
2022-04-28 20:17:22 +0200

    fix debug

--- FHEM/74_GardenaSmartDevice.pm
+++ FHEM/74_GardenaSmartDevice.pm
@@ -784,22 +784,6 @@ sub WriteReadings {
               readingsBulkUpdateIfChanged( $hash, 'scheduling-'.$decode_json->{settings}[$settings]{name},
                                   $decode_json->{settings}[$settings]{value} );
             }
-            if ($decode_json->{settings}[$settings]{name} eq 'valve_names') {
-              Log3 $name, 3,  "[DEBUG] - GardenaSmartDevice ($name) - ".ref( $decode_json->{settings}[$settings]{value} );
-            }
-
-            if ($decode_json->{settings}[$settings]{name} eq 'valve_names'
-                && ref( $decode_json->{settings}[$settings]{value} ) eq "ARRAY" ) { # or HASH ?
-              #_name_1  =  
-              Log3 $name, 3,  "GardenaSmartDevice ($name) valve_names ";
-              my @valves = @{$decode_json->{settings}[$settings]{value}};
-              foreach my $valve( @valves ) {
-                Log3 $name, 3,  "GardenaSmartDevice ($name) valve_name $valve->{'name'}";
-
-              readingsBulkUpdateIfChanged( $hash, 'valve-valve_name_'.$valve->{"id"},
-                                                  $valve->{"name"} );
-              }
-            }
 
             ######
             # save winter mode as reading
@@ -808,6 +792,22 @@ sub WriteReadings {
                     $decode_json->{settings}[$settings]{value} );
 
                 $winter_mode = $decode_json->{settings}[$settings]{value};
+            }
+        }
+
+        if ($decode_json->{settings}[$settings]{name} eq 'valve_names') {
+            Log3 $name, 3,  "[DEBUG] - GardenaSmartDevice ($name) - ".ref( $decode_json->{settings}[$settings]{value} );
+        }
+
+        if ($decode_json->{settings}[$settings]{name} eq 'valve_names'
+          && ref( $decode_json->{settings}[$settings]{value} ) eq "ARRAY" ) { # or HASH ?
+          #_name_1  =  
+            Log3 $name, 3,  "GardenaSmartDevice ($name) valve_names ";
+            my @valves = @{$decode_json->{settings}[$settings]{value}};
+            foreach my $valve( @valves ) {
+              Log3 $name, 3,  "GardenaSmartDevice ($name) valve_name $valve->{'name'}";
+              readingsBulkUpdateIfChanged( $hash, 'valve-valve_name_'.$valve->{"id"},
+                                           $valve->{"name"} );
             }
         }
 
